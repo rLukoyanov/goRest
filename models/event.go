ee85a8af1c2344d36a4133901bed747a4497a1d6
Ruslan Lukoyanov <ruslan.ru226@mail.ru>
2025-02-23 20:31:11 +0400

    main: add creating post

--- models/event.go
+++ models/event.go
@@ -4,13 +4,20 @@ import "time"
 
 type Event struct {
 	Id          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UserId      int
 }
 
+var events = []Event{}
+
 func (event Event) Save() {
 	// todo: add to db
+	events = append(events, event)
+}
+
+func GetAllEvents() []Event {
+	return events
 }
